323b74f69785fb08c82553ec45a7fcca01ae8498
Jeff H <jahuangi@yahoo.ca>
2015-08-24 09:53:06 -0700

    initial check-in

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,19 @@
+class RequestsController < ApplicationController
+  def new,
+  end
+
+  def create,
+  end
+
+  def show,
+  end
+
+  def update,
+  end
+
+  def index,
+  end
+
+  def destroy
+  end
+end
